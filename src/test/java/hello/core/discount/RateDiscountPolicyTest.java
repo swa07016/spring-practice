622347223ef863fd46c78a63fdd8db29cf81dae4
swa07016 <swa07016@naver.com>
2020-09-28 22:03:27 +0900

    Apply AppConfig

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -22,7 +22,7 @@ class RateDiscountPolicyTest {
         //then
         assertThat(discount).isEqualTo(1000);
     }
-    
+
     @Test
     @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
     void vip_x() {
